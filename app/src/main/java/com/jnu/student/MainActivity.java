344688a1b92b2af34319310bbb056c8731a3729d
CoconutL26 <2460045654@qq.com>
2023-12-05 18:43:56 +0800

    移动开发实验二

---
+++ app/src/main/java/com/jnu/student/MainActivity.java
@@ -0,0 +1,16 @@
+package com.jnu.student;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //String helloWorld = getResources().getString(R.string.hello_world);
+    }
+
+}
