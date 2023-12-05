9e106ccd8ff8bf8c516f215184b6704553110c21
CoconutL26 <2460045654@qq.com>
2023-12-05 18:59:52 +0800

    移动开发实验四

--- app/src/main/java/com/jnu/student/MainActivity.java
+++ app/src/main/java/com/jnu/student/MainActivity.java
@@ -1,8 +1,11 @@
 package com.jnu.student;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,8 +15,26 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView textView = findViewById(R.id.text_vciew_hellow_world);
-        textView.setText(getString(R.string.HW));
-    }
+        TextView textView1 = findViewById(R.id.textView1);
+        TextView textView2 = findViewById(R.id.textView2);
+        Button button = findViewById(R.id.button);
+        textView1.setText(getString(R.string.hello_world));
+        textView2.setText(getString(R.string.jnu));
 
+        button.setOnClickListener(v -> {
+            String text1 = textView1.getText().toString();
+            String text2 = textView2.getText().toString();
+
+            textView1.setText(text2);
+            textView2.setText(text1);
+
+            Toast.makeText(MainActivity.this, "交换成功", Toast.LENGTH_SHORT).show();
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setMessage("交换成功")
+                    .setPositiveButton("确定", (dialog, id) -> dialog.dismiss());
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        });
+    }
 }
